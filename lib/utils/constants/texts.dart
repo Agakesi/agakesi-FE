8d98b1f1a098f5fa3b34dd31cb3f09f99666d2fa
Sam <s.olubode@alustudent.com>
2024-07-06 13:58:11 +0100

    chore: utils - set up themes, colors, sizes

---
+++ lib/utils/constants/texts.dart
@@ -0,0 +1,17 @@
+class ChindiTexts {
+  ChindiTexts._();
+
+  /// AUTH
+  static const String signupHeaderText = 'Let\'s create your account';
+
+  /// PROFILE
+  static const String profile = 'Profile';
+  static const String firstname = 'Firstname';
+  static const String lastname = 'Lastname';
+  static const String email = 'Email';
+  static const String address = 'Address';
+  static const String support = 'Support';
+
+  /// CHAT
+  static const String today = 'Today';
+}
---
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import '../../constants/colors.dart';
+import '../../constants/sizes.dart';
+
+/// App Elevated Button Theme
+class AppElevatedButtonTheme {
+  AppElevatedButtonTheme._(); //To avoid creating instances
+
+  /// Light Theme - Elevation Button
+  static final lightElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: AppColors.light,
+      backgroundColor: AppColors.primary,
+      disabledForegroundColor: AppColors.darkGrey,
+      disabledBackgroundColor: AppColors.buttonDisabled,
+      side: BorderSide(color: AppColors.primary),
+      padding: const EdgeInsets.symmetric(vertical: AppSizes.buttonHeight),
+      textStyle: const TextStyle(
+          fontSize: 16,
+          color: AppColors.textWhite,
+          fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSizes.buttonRadius)),
+    ),
+  );
+
+  /// Dark Theme - Elevation Button
+  static final darkElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: AppColors.light,
+      backgroundColor: AppColors.primary,
+      disabledForegroundColor: AppColors.darkGrey,
+      disabledBackgroundColor: AppColors.darkerGrey,
+      side: BorderSide(color: AppColors.primary),
+      padding: const EdgeInsets.symmetric(vertical: AppSizes.buttonHeight),
+      textStyle: const TextStyle(
+          fontSize: 16,
+          color: AppColors.textWhite,
+          fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSizes.buttonRadius)),
+    ),
+  );
+}
